5874b97e4de4c10c6a05b7a21d26eca1ddbfba76
sunning1997 <43701593+sunning1997@users.noreply.github.com>
2021-08-10 21:31:17 +0800

    Update javaTest.java

--- javaTest.java
+++ javaTest.java
@@ -2,5 +2,6 @@ class javaTest{
 	public static void main(String[] args){
 		char[] newChar = new char[3]{"s","a","d"};
 		System.out.println(newChar[1]-'a');
+		System.out.println(newChar[2]-'a');
 	}
 }
