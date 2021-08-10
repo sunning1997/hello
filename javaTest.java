6fcfb355c2c6226452749e56c37bdb57316453d0
sunning <1690016787@qq.com>
2021-08-10 22:17:42 +0800

    update javaTest,java

--- javaTest.java
+++ javaTest.java
@@ -1,6 +1,7 @@
 class javaTest{
 	public static void main(String[] args){
 		char[] newChar = new char[3]{"s","a","d"};
+		System.out.println(newChar[0]-'a');
 		System.out.println(newChar[1]-'a');
 		System.out.println(newChar[2]-'a');
 	}
